0e01f4ae7fd0fbe09e3d4488cf9960004814199c
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-08-28 19:35:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,3 +2,16 @@ class Song
   attr accessor :title
   
   def initilize(name)
+    @title = title
+  end
+  
+def add_song(song)
+  @songs << song
+end
+
+end
+
+a = Artist.new
+a.songs << Song.new
+
+a.add_song(Song.new)
